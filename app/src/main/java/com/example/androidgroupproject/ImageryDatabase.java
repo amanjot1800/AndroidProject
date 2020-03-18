74002b6fd842d47a62f2e710e1b9f868055df585
shubham <shar0514@algonquinlive.com>
2020-03-18 11:44:37 -0400

    commit

--- app/src/main/java/com/example/androidgroupproject/ImageryDatabase.java
+++ app/src/main/java/com/example/androidgroupproject/ImageryDatabase.java
@@ -6,16 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ImageryDatabase extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button ns = findViewById(R.id.nasa);
-        ns.setOnClickListener(click ->{
-            Intent nasa = new Intent(this, ImageryDatabase.class);
-            startActivity(nasa);
-        });
+        setContentView(R.layout.activity_imagery_database);
+
     }
 }
