d1558c5fc3196aa39a0da3f268bf7fd64b11eb01
Jashan <prut0002@algonquinlive.com>
2020-03-24 19:42:27 -0400

    Milestone1(snack, toast)

--- app/src/main/java/com/example/androidgroupproject/Guardian.java
+++ app/src/main/java/com/example/androidgroupproject/Guardian.java
@@ -19,6 +19,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +28,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -38,7 +40,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class Guardian extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class Guardian extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ArrayList<Article> articles = new ArrayList<>();
     private MyAdapter adapter;
@@ -54,7 +56,7 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
         Toolbar tBar = findViewById(R.id.toolBar);
         setSupportActionBar(tBar);
         DrawerLayout drawer = findViewById(R.id.drawerLayout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, tBar, R.string.open,R.string.close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tBar, R.string.open, R.string.close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -72,17 +74,17 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
             articles.clear();
             adapter.notifyDataSetChanged();
             Scraper thread = new Scraper();
-           // if (!topic.getText().toString().isEmpty())
-                thread.execute("https://content.guardianapis.com/search?api-key=1fb36b70-1588-4259-b703-2570ea1fac6a&q="+topic.getText().toString());
-//            else
-//                Snackbar.make(null, "Please enter a Topic to search", Snackbar.LENGTH_LONG).show();
+            if (!topic.getText().toString().isEmpty())
+                thread.execute("https://content.guardianapis.com/search?api-key=1fb36b70-1588-4259-b703-2570ea1fac6a&q=" + topic.getText().toString());
+            else
+                Toast.makeText(this, "Please enter a Topic to search", Toast.LENGTH_LONG).show();
         });
 
         //listView.setOnItemLongClickListener();
-        listView.setOnItemClickListener((parent, view, pos, id)->{
+        listView.setOnItemClickListener((parent, view, pos, id) -> {
             Intent goToArticleDetail = new Intent(Guardian.this, ArticleDetail.class);
             Article selectedArticle = articles.get(pos);
-            goToArticleDetail.putExtra("title",selectedArticle.getTitle());
+            goToArticleDetail.putExtra("title", selectedArticle.getTitle());
             goToArticleDetail.putExtra("url", selectedArticle.getUrl());
             goToArticleDetail.putExtra("sectionName", selectedArticle.getSectionName());
             goToArticleDetail.putExtra("id", selectedArticle.getId());
@@ -102,19 +104,18 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
 //        });
     }
 
-    protected void deleteArticle(Article article)
-    {
-        db.delete(MyOpener.TABLE_NAME, MyOpener.COL_ID + "= ?", new String[] {Long.toString(article.getId())});
+    protected void deleteArticle(Article article) {
+        db.delete(MyOpener.TABLE_NAME, MyOpener.COL_ID + "= ?", new String[]{Long.toString(article.getId())});
     }
-    private void loadDataFromDatabase()
-    {
+
+    private void loadDataFromDatabase() {
         //get a database connection:
         MyOpener dbOpener = new MyOpener(this);
         db = dbOpener.getWritableDatabase();
 
 
         // We want to get all of the columns. Look at MyOpener.java for the definitions:
-        String [] columns = {MyOpener.COL_ID, MyOpener.COL_TITLE, MyOpener.COL_URL, MyOpener.COL_SECT};
+        String[] columns = {MyOpener.COL_ID, MyOpener.COL_TITLE, MyOpener.COL_URL, MyOpener.COL_SECT};
         //query all the results from the database:
         results = db.query(false, MyOpener.TABLE_NAME, columns, null, null, null, null, null, null);
 
@@ -126,8 +127,7 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
         int idColIndex = results.getColumnIndex(MyOpener.COL_ID);
 
         //iterate over the results, return true if there is a next item:
-        while(results.moveToNext())
-        {
+        while (results.moveToNext()) {
             String title = results.getString(titleColumnIndex);
             String url = results.getString(urlColIndex);
             String sect = results.getString(sectColIndex);
@@ -141,10 +141,9 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
     }
 
     @Override
-    public boolean onNavigationItemSelected( MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item) {
 
-        switch(item.getItemId())
-        {
+        switch (item.getItemId()) {
             case R.id.guardian:
                 Intent goToGuardian = new Intent(Guardian.this, Guardian.class);
                 startActivity(goToGuardian);
@@ -168,6 +167,7 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
         // Toast.makeText(this, "NavigationDrawer: " + message, Toast.LENGTH_LONG).show();
         return true;
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -177,8 +177,7 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId())
-        {
+        switch (item.getItemId()) {
             case R.id.guardian:
                 Intent goToGuardian = new Intent(Guardian.this, Guardian.class);
                 startActivity(goToGuardian);
@@ -194,8 +193,9 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
             case R.id.BBC:
 //                Intent goToChat = new Intent(TestToolBar.this, chatRoomActivity.class);
 //                startActivity(goToChat);
-                break;}
-            return true;
+                break;
+        }
+        return true;
     }
 
     private class Scraper extends AsyncTask<String, Integer, String> {
@@ -229,7 +229,7 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
                     newRowValues.put(MyOpener.COL_SECT, sectionName);
                     //Now insert in the database:
                     long newId = db.insert(MyOpener.TABLE_NAME, null, newRowValues);
-                    Article article = new Article(title, articleUrl, sectionName,newId);
+                    Article article = new Article(title, articleUrl, sectionName, newId);
                     articles.add(article);
                 }
                 publishProgress(100);
@@ -244,8 +244,10 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
 
         @Override
         protected void onProgressUpdate(Integer... values) {
-            progressBar.setVisibility(View.VISIBLE);
-            progressBar.setProgress(100);
+            if (articles.isEmpty())
+                Snackbar.make(findViewById(R.id.list), "No Articles found", Snackbar.LENGTH_LONG).show();
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setProgress(100);
         }
 
         @Override
@@ -271,7 +273,7 @@ public class Guardian extends AppCompatActivity implements NavigationView.OnNavi
             LayoutInflater inflater = getLayoutInflater();
             View newView = inflater.inflate(R.layout.article_layout, parent, false);
             TextView tView = newView.findViewById(R.id.tf);
-            tView.setText((position+1)+". "+getItem(position).getTitle());
+            tView.setText((position + 1) + ". " + getItem(position).getTitle());
             return newView;
         }
 
