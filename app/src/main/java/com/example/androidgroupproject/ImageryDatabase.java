988f8a2c5e97209b3a4a5c0fbb6e2902887a4f87
shubham <shar0514@algonquinlive.com>
2020-03-31 10:46:49 -0400

    comited

--- app/src/main/java/com/example/androidgroupproject/ImageryDatabase.java
+++ app/src/main/java/com/example/androidgroupproject/ImageryDatabase.java
@@ -8,6 +8,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,23 +17,8 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.example.androidgroupproject.R;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Date;
 
 public class ImageryDatabase extends AppCompatActivity {
     EditText latitude;
@@ -44,8 +31,13 @@ public class ImageryDatabase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagery_database);
+        androidx.appcompat.widget.Toolbar tBar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+
+        //This loads the toolbar, which calls onCreateOptionsMenu below:
+        setSupportActionBar(tBar);
         latitude = findViewById(R.id.lati);
         longitude = findViewById(R.id.loni);
+
 
         Button btn = findViewById(R.id.subm);
         btn.setOnClickListener(click -> {
@@ -65,8 +57,15 @@ public class ImageryDatabase extends AppCompatActivity {
             Intent q = new Intent(this, MainDatabase.class);
             startActivity(q);
         });
-    }
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 
 
 }
